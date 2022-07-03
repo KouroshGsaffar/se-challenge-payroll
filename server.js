72da45ae2a2c376ca9434e7694a7c4575dffb115
KouroshGSaffar <kooroshgharooni74@gmail.com>
2022-07-03 16:39:45 -0400

    adding middleware to parse the parse data

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express=require('express')  
-const app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// setup morgan as logger to log request details
+app.use(morgan('dev'));
+
+// adding middleware for parsing bodies from URL and parsing json objects
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var PORT = process.env.PORT || 3000;
   
 app.get('/',(req,res) => {
     res.send('Welcome Page');
 })
   
-const PORT = 3001;
+var PORT = process.env.PORT || 3001;
   
 app.listen(PORT,() => {
     console.log(`Running on PORT ${PORT}`);
